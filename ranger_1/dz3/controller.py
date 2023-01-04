6b2281dd271161ae2d4c05c2fc91687d19cb1bc5
Denis <xdenis@yandex.ru>
2023-01-04 18:20:13 +0300

    Добавлен файл controller

---
+++ ranger_1/dz3/controller.py
@@ -0,0 +1,26 @@
+import addContact as ac
+import userBook as ui
+import exportFile as f
+import deletContact as d
+import viewContact as v
+
+
+def user_controller():
+
+    num = ui.menu()
+    if num < 0 or num > 6:
+        print('\nОшибка ввода!\n\nЧисло должно соответствовать пункту меню!\n')
+        user_controller()
+    elif num == 1:
+        ac.create()
+    elif num == 2:
+        ac.add()
+    elif num == 3:
+        f.export_txt()
+    elif num == 4:
+        d.delete_contact()
+    elif num == 5:
+        v.view_all_contact()
+    elif num == 0:
+        print('\nВыход из приложения. Пока!')
+        exit()
